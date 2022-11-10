1df0ee4dc78693b6c22185bb35760dfcbe137d25
rajpranit <rajpranit1207@gmail.com>
2022-11-10 22:51:07 +0530

    Auht context and provider

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,4 +1,5 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import { AuthContext } from "../../store/auth-context";
 
 import classes from "./AuthForm.module.css";
 
@@ -6,6 +7,8 @@ const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -42,12 +45,15 @@ const AuthForm = () => {
         if (res.ok) {
           return res.json();
         } else {
-          return res.json().then((data) => console.log(data));
+          return res.json().then((data) => {
+            throw new Error(data.error.message);
+          });
         }
       })
-      .then((data) => console.log(data))
+      .then((data) => authCtx.login(data.idToken))
       .catch((err) => console.log(err));
   };
+  console.log(authCtx.isLoggedIn);
 
   return (
     <section className={classes.auth}>
---
+++ src/store/auth-context.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import React from "react";
+
+export const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  const userIsLoggedIn = !!token;
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = (token) => {
+    setToken(token);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
